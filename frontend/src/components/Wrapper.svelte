2ba12868d70f4e4dc16af2335e26865b2aca79da
Till Prochaska <mail@tillprochaska.de>
2021-11-08 16:56:56 +0100

    Initial commit

---
+++ frontend/src/components/Wrapper.svelte
@@ -0,0 +1,9 @@
+<style>
+  .wrapper {
+   max-width: var(--max-width);
+  }
+</style>
+
+<div class="wrapper">
+  <slot />
+</div>
---
+++ frontend/src/routes/topics.svelte
@@ -0,0 +1,7 @@
+<script>
+  import Wrapper from '../components/Wrapper.svelte';
+</script>
+
+<Wrapper>
+  <h1 class="alpha">What the European Parliament Votes On</h1>
+</Wrapper>
